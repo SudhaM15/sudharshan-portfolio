d6ee13f16d3f1367bbbc75332db1bee124369205
SudhaM15 <Sudharshan2515@gmail.com>
2023-09-05 20:21:28 +0530

    adjust carousel

---
+++ src/main.js
@@ -0,0 +1,32 @@
+curentSlideID = 1;
+
+sliderElement = document.getElementById("slider");
+
+totalSlides = 4;
+console.log(totalSlides);
+
+function next() {
+  if (curentSlideID < totalSlides) {
+    curentSlideID++;
+    showSlide();
+  }
+}
+
+function prev() {
+  if (curentSlideID > 1) {
+    curentSlideID--;
+    showSlide();
+  }
+}
+
+function showSlide() {
+  slides = document.getElementById("slider").getElementsByTagName("li");
+  for (let index = 0; index < totalSlides; index++) {
+    const element = slides[index];
+    if (curentSlideID === index + 1) {
+      element.classList.remove("hidden");
+    } else {
+      element.classList.add("hidden");
+    }
+  }
+}
