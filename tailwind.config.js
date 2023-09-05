a1fbcf13d3744020e7c10adecdb40d00fc9b1218
SudhaM15 <Sudharshan2515@gmail.com>
2023-09-05 16:34:58 +0530

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kav: "'Kaushan Script', cursive;",
+        oswald: "'Oswald', sans-serif;",
+        sand: "'Quicksand', sans-serif;",
+      },
+    },
+  },
+  plugins: [],
+};
+
